714980df42d1e717568752a3b3f36d96f6f35ef1
anja.ivanovic <anja.ivanovic02kg@gmail.com>
2023-02-25 16:13:20 +0100

    API osnovne funkcije (MongoDB)

---
+++ sandbox/AnjaIvanovic/back/Controllers/BooksController.cs
@@ -0,0 +1,76 @@
+﻿using back.Models;
+using back.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly BooksService booksService;
+
+        public BooksController(BooksService booksService)
+        {
+            this.booksService = booksService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> getAllBooks()
+        {
+            var books = await booksService.GetAllBooksAsync();
+            return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("{id:length(24)}")]
+        [ActionName("getBook")]
+        public async Task<IActionResult> getBook([FromRoute] string id)
+        {
+            var book = await booksService.GetBookAsync(id);
+            if (book != null)
+                return Ok(book);
+            return NotFound("Book not found.");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> addBook([FromBody] Book book)
+        {
+            book.Id = ObjectId.GenerateNewId().ToString();
+            await booksService.InsertBookAsync(book);
+
+            return CreatedAtAction(nameof(getBook), new { id = book.Id }, book);
+        }
+
+        [HttpPut]
+        [Route("{id:length(24)}")]
+        public async Task<IActionResult> updateBook([FromRoute] string id, [FromBody] Book bookUpdate)
+        {
+            var book = await booksService.GetBookAsync(id);
+            if (book != null)
+            {
+                bookUpdate.Id = book.Id;
+                book = bookUpdate;
+                await booksService.UpdateBookAsync(id, book);
+                return NoContent();
+            }
+            return NotFound("Book not found.");
+
+        }
+
+        [HttpDelete]
+        [Route("{id:length(24)}")]
+        public async Task<IActionResult> deleteBook([FromRoute] string id)
+        {
+            var book = await booksService.GetBookAsync(id);
+            if (book != null)
+            {
+                await booksService.RemoveBookAsync(id);
+                return NoContent();
+            }
+            return NotFound("Book not found.");
+        }
+    }
+}
--- sandbox/AnjaIvanovic/back/Program.cs
+++ sandbox/AnjaIvanovic/back/Program.cs
@@ -1,4 +1,6 @@
 using back.Data;
+using back.Models;
+using back.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +12,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//sqlite connection
 builder.Services.AddDbContext<GamesDbContext>(options =>
 options.UseSqlite(builder.Configuration.GetConnectionString("GamesConnectionString")));
+
+//mongodb connection
+builder.Services.Configure<booksDbSettings>(
+    builder.Configuration.GetSection("booksDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 var app = builder.Build();
 
