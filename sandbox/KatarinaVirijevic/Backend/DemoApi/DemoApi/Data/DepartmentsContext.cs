165d24af6202a405d48f49bc938b4824e2c8d610
KatVir <94877518+KatVir@users.noreply.github.com>
2023-02-26 23:19:44 +0100

    #2 uspostavljena konekcija sa sqlite bazom i get zahtevi

---
+++ sandbox/KatarinaVirijevic/Backend/DemoApi/DemoApi/Data/DepartmentsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoApi.Models;
+
+namespace DemoApi.Data
+{
+    public class DepartmentsContext : DbContext
+    {
+        public DepartmentsContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
