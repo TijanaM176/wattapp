682a5bbfcb2c75e4830192ff2dc90c0793ed34c2
lukaJevtic1 <114006215+lukaJevtic1@users.noreply.github.com>
2023-03-02 00:34:22 +0100

    #3

---
+++ sandbox/LukaJevtic/API/Studenti/Studenti.API/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Studenti.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//inject
+builder.Services.AddDbContext<StudentDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentiConnectionString")));
+
+
+builder.Services.AddCors((setup) =>
+{
+    setup.AddPolicy("default", (options) =>
+    {
+        options.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    }
+    );
+}
+);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("default");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
