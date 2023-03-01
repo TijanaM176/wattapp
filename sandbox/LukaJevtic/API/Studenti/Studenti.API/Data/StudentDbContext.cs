90800f6971296281e309b111d205d6f475ef91d3
lukaJevtic1 <114006215+lukaJevtic1@users.noreply.github.com>
2023-03-02 00:33:17 +0100

    #3

---
+++ sandbox/LukaJevtic/API/Studenti/Studenti.API/Data/StudentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studenti.API.Models;
+
+namespace Studenti.API.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> dajSveStudente { get; set; }
+    }
+}
