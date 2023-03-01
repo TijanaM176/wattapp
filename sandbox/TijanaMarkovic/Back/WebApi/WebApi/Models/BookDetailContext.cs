cc8234a006cb0b954e2849205ca0b73293a336dd
TijanaM176 <tijanamarkovic176@gmail.com>
2023-03-01 14:06:22 +0100

    Dodato

---
+++ sandbox/TijanaMarkovic/Back/WebApi/WebApi/Models/BookDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class BookDetailContext : DbContext
+    {
+        public BookDetailContext(DbContextOptions<BookDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BookDetail> BookDetails { get; set; }
+    }
+}
