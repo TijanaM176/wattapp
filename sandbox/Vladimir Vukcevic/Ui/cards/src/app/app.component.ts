531fbd9544b20a6ccfa368b81d2eb1849ef1ae47
Vladimir <vukcevic799@gmail.com>
2023-03-02 00:07:37 +0100

    Faza3

--- sandbox/Vladimir Vukcevic/Ui/cards/src/app/app.component.ts
+++ sandbox/Vladimir Vukcevic/Ui/cards/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from './models/card.model';
+import { CardsService } from './service/cards.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cards';
+  cards: Card[]=[];
+  card: Card={
+    id: '',
+    cardNumber: '',
+    cardHolderName: '',
+    cvc: '',
+    expiryMonth: '',
+    expiryYear: ''
+  }
+  constructor(private cardsService: CardsService)
+  {
+
+  }
+  ngOnInit(): void {
+   this.getAllCards();
+  }
+
+  getAllCards()
+  {
+    this.cardsService.getAllCards()
+    .subscribe(
+       response => {
+         this.cards=response;
+      }
+      );
+  }
+  onSubmit(){
+    console.log(this.card);
+  }
+
 }
