c64daff88083861948a87da415f788450a0d91c4
KatVir <94877518+KatVir@users.noreply.github.com>
2023-03-08 00:01:06 +0100

    backend
    Isprobavam na koji nacin se radi login koriscenjem angulara. Kako je veci fokus na frontend-u, nisam radila stvari na backendu poput hesiranja sifre i sl

---
+++ sandbox/KatarinaVirijevic/LoginProba/loginProbaBackend/loginProbaBackend/Controllers/UsersController.cs
@@ -0,0 +1,86 @@
+﻿using loginProbaBackend.Data;
+using loginProbaBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace loginProbaBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getAllUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> getUserByUsername([FromBody] LoginDto loginUser)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginUser.username);
+            if(user is null)
+            {
+                return BadRequest(new
+                {
+                    error = true,
+                    message = "User Not Found!"
+                });
+            }
+            else
+            {
+                if(user.Password==loginUser.password) 
+                {
+                    return Ok(new
+                    {
+                        error = false,
+                        message = loginUser.username
+                    });
+                }
+                return BadRequest(new
+                {
+                    error = true,
+                    message = "Incorrect Password!"
+                });
+            }
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> addUser([FromBody] NewUser newUser)
+        {
+            User user = new User();
+            user.Username = newUser.Username;
+            user.Password = newUser.Password;
+            user.Name = newUser.Name;
+            user.Id = new Guid();
+
+            try
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return Ok(new
+                {
+                    error = false,
+                    message = user.Username
+                });
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new
+                {
+                    error = true,
+                    message = "Error Occurred!"
+                });
+            }
+        }
+
+    }
+}
---
+++ sandbox/KatarinaVirijevic/LoginProba/loginProbaBackend/loginProbaBackend/Models/NewUser.cs
@@ -0,0 +1,9 @@
+﻿namespace loginProbaBackend.Models
+{
+    public class NewUser
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
