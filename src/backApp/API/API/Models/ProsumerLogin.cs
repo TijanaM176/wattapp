f78f660be63e0af0e47d77e5f427fd3810baa60a
anja.ivanovic <anja.ivanovic02kg@gmail.com>
2023-03-08 17:16:08 +0100

    Login - provera i generisanje tokena

---
+++ src/backApp/API/API/Models/ProsumerLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models
+{
+    public class ProsumerLogin
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
