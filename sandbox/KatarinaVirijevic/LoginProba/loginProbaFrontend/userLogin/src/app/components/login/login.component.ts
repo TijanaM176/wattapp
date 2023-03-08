6f290faeec60f5ef8c7352243cee51324dce5bb7
KatVir <94877518+KatVir@users.noreply.github.com>
2023-03-08 22:22:13 +0100

    validacija forme za login

--- sandbox/KatarinaVirijevic/LoginProba/loginProbaFrontend/userLogin/src/app/components/login/login.component.ts
+++ sandbox/KatarinaVirijevic/LoginProba/loginProbaFrontend/userLogin/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit{
   
-  type: string ="password"
-  isText: boolean = false
-  eyeIcon: string = "fa-eye-slash"
-  constructor() { }
+  type: string ="password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
   
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    })
   }
 
   hideShowPass()
@@ -20,5 +26,35 @@ export class LoginComponent implements OnInit{
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
     this.isText ? this.type = 'text' : this.type = 'password';
+  }
+
+  onSubmit()
+  {
+    if(this.loginForm.valid)
+    {
+      console.log(this.loginForm.value);
+      //poslati beku
+    }
+    else
+    {
+      //console.log("Form is not valid!");
+      this.validateAllFormFields(this.loginForm);
+      //alert("Your form is invalid!");
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup)
+  {
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl)
+      {
+        control.markAsDirty({onlySelf:true});
+      }
+      else if(control instanceof FormGroup)
+      {
+        this.validateAllFormFields(control);
+      }
+    })
   }
 }
