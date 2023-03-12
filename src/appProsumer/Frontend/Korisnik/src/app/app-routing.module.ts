0b40a360acfda956c0003352224630ae55473512
KatVir <94877518+KatVir@users.noreply.github.com>
2023-03-12 18:24:57 +0100

    #26 dodat guard koji obezbedjuje da ne mozemo pristupiti ostatku aplikacije bez logovanja

--- src/appProsumer/Frontend/Korisnik/src/app/app-routing.module.ts
+++ src/appProsumer/Frontend/Korisnik/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import{LoginComponent} from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
+
 const routes: Routes = [
   {path:'login', component:LoginComponent},
-  {path:'dashboard', component:DashboardComponent},
+  {path:'dashboard', component:DashboardComponent, canActivate: [AuthGuard]},
   {path:"", redirectTo:"/dashboard",pathMatch: "full"}
 ];
 
