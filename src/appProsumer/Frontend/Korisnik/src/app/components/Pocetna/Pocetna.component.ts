23a98876b1cb79c14a981c000a1eb8e7513524c0
Mihailo <sebekmihailo@gmail.com>
2023-03-13 20:32:48 +0100

    #40 navbar i sidebar placeholderi

---
+++ src/appProsumer/Frontend/Korisnik/src/app/components/Pocetna/Pocetna.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Pocetna',
+  templateUrl: './Pocetna.component.html',
+  styleUrls: ['./Pocetna.component.css']
+})
+export class PocetnaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
