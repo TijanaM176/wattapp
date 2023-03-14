aff1df302a4ef4375518d9f82f52e20604861451
KatVir <94877518+KatVir@users.noreply.github.com>
2023-03-14 17:50:40 +0100

    #37 responsive izgled dso login stranice
    izmene napravljene po ugledu na novu verziju iz figme

--- src/appDistributer/Frontend/DSO/src/app/app.module.ts
+++ src/appDistributer/Frontend/DSO/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgToastModule } from 'ng-angular-popup';
+import { CookieService } from 'ngx-cookie-service';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
-  providers: [],
+  declarations: [
+    AppComponent, 
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule, 
+    HttpClientModule, 
+    FormsModule, 
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgToastModule
+  ],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
