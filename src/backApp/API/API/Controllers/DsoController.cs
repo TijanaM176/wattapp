309cc318cf7979708d38896c2f2985531f92fa95
anja.ivanovic <anja.ivanovic02kg@gmail.com>
2023-03-19 21:25:15 +0100

    Sitna ispravka

--- src/backApp/API/API/Controllers/DsoController.cs
+++ src/backApp/API/API/Controllers/DsoController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DsoController : Controller
     {
         private readonly IDsoService dsoService;
