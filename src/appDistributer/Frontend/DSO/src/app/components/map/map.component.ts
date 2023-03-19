5c7cd0fb58a826bf152d1da59ea146ba66a1659a
KatVir <94877518+KatVir@users.noreply.github.com>
2023-03-19 21:42:39 +0100

    #53 dodato pracenje korisnikove lokacije i upozorenje ukoliko je doslo do nekih smetnji
    takodje je izmenjena slike markera za trenutnu lokaciju radnika dso-a

--- src/appDistributer/Frontend/DSO/src/app/components/map/map.component.ts
+++ src/appDistributer/Frontend/DSO/src/app/components/map/map.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { MapService } from 'src/app/services/map.service';
 import { NgToastService } from 'ng-angular-popup';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-map',
@@ -12,10 +13,18 @@ export class MapComponent implements AfterViewInit, OnInit {
   
   map: any;
   users: any;
+  currentLocation:any;
+  currentLocationIsSet = false;
 
-  constructor(private mapService : MapService, private toast: NgToastService) {}
+  constructor(private mapService : MapService, private toast: NgToastService, private cookie: CookieService) {}
   
   ngOnInit(): void {
+    if(this.cookie.check('lat'))
+    {
+      this.cookie.delete('lat');
+      this.cookie.delete('long');
+    }
+    this.currentLocationIsSet = false
     //this.getUsers();
   }
   
@@ -25,30 +34,57 @@ export class MapComponent implements AfterViewInit, OnInit {
   }
 
   private initMap(){
-    this.map = L.map('map').setView([44.012794, 20.911423],13);
+    this.map = L.map('map').setView([44.012794, 20.911423],15);
     const tiles = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       } as L.TileLayerOptions)
 
     tiles.addTo(this.map);
     const defaultIcon = L.icon({
-      iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
-      shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+      iconUrl: 'assets/images/location.svg',//'https://leafletjs.com/examples/custom-icons/leaf-green.png',
+      //shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+      iconSize: [30,30],
       shadowSize:   [50, 64], 
       iconAnchor:   [22, 94], 
       shadowAnchor: [4, 62],  
-      popupAnchor:  [-3, -76]
+      popupAnchor:  [-8, -93]
     })
-    this.map.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
+
+    this.map.locate({setView: true, watch: true, enableHighAccuracy: true, timeout: 50}) /* This will return map so you can do chaining */
     .on('locationfound', (e:any) =>{
-        var marker = L.marker([e.latitude, e.longitude],{icon: defaultIcon}).bindPopup('Your are here');
-        marker.addTo(this.map)
+        //console.log(e);
+        if(this.currentLocationIsSet)
+        {
+          //console.log("izbrisana prethodna lokacija");
+          this.map.removeLayer(this.currentLocation);
+        }
+        this.cookie.set('lat',e.latitude);
+        this.cookie.set('long',e.longitude);
+        var acc = Number(e.accuracy).toFixed(2);
+        this.currentLocation = L.marker([e.latitude,e.longitude],{icon: defaultIcon}).bindPopup('Your are here.<br>(within '+acc+' meters from this point)');
+        this.currentLocation.addTo(this.map);
+        this.currentLocationIsSet=true;
     })
    .on('locationerror', (e:any) =>{
         console.log(e);
-        this.toast.error({detail:"ERROR", summary: "Location access denied.", duration: 3000});
+        this.toast.error({detail:"ERROR", summary: "Unable To Update Your Current Location.", duration: 3000});
         //alert("Location access denied.");
     });
+    
+    const findMeControl = L.Control.extend({
+      options: {
+        position: 'topleft'
+      },
+      onAdd: () => {
+        const button = L.DomUtil.create('button');
+        button.innerHTML = '<span class="fa fa-crosshairs p-1"></span>';
+        button.addEventListener('click', () => {
+          this.map.setView([this.cookie.get('lat'), this.cookie.get('long')],15);
+        });
+        return button;
+      }
+    });
+    this.map.addControl(new findMeControl());
   }
   //drugi nacin
   /*options: L.MapOptions = {
