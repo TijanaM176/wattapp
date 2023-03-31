f558dd6b2e7dd3df7c2cb0bbabb8c88551ed23c8
KatVir <94877518+KatVir@users.noreply.github.com>
2023-04-01 00:48:23 +0200

    #97 spojene sve komponente, zavrsen izgled stranice za korisnika i omogucena promena menija u zavisnosti od sirine uredjaja na kojoj se aplikacija otvara

--- src/appProsumer/Frontend/Korisnik/src/app/components/dashboard/dashboard.component.ts
+++ src/appProsumer/Frontend/Korisnik/src/app/components/dashboard/dashboard.component.ts
@@ -1,24 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthServiceService } from 'src/app/services/auth-service.service';
 import { NgToastService } from 'ng-angular-popup';
+import { DeviceWidthService } from 'src/app/services/device-width.service';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit, OnDestroy{
 
-  constructor(private router: Router, private cookie: CookieService, private auth: AuthServiceService, private toast: NgToastService) {}
+  deviceWidth!: number;
+  resizeObservable$!: Observable<Event>
+  resizeSubscription$!: Subscription
+
+  constructor(private router: Router, private cookie: CookieService, private auth: AuthServiceService, private toast: NgToastService, private widthService : DeviceWidthService) {}
 
   ngOnInit(): void {
+    this.deviceWidth = this.widthService.deviceWidth;
+    this.resizeObservable$ = fromEvent(window, 'resize');
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
+      this.widthService.deviceWidth = window.innerWidth;
+      this.deviceWidth = this.widthService.deviceWidth;
+    });
   }
-  
-  LogOut()
-  {
-    this.cookie.deleteAll();
-    this.router.navigate(["login"]);
+
+  ngOnDestroy(): void {
+    this.resizeSubscription$.unsubscribe();
   }
+
+  // @HostListener('window:resize',['$event'])
+  // onResize(evet : any){
+  //   this.widthService.deviceWidth = evet.target.innerWidth;
+  // }
+
 }
--- src/appProsumer/Frontend/Korisnik/src/app/components/sidebar-responsive/sidebar-responsive.component.ts
+++ src/appProsumer/Frontend/Korisnik/src/app/components/sidebar-responsive/sidebar-responsive.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
 
 @Component({
   selector: 'app-sidebar-responsive',
   templateUrl: './sidebar-responsive.component.html',
   styleUrls: ['./sidebar-responsive.component.css']
 })
-export class SidebarResponsiveComponent {
+export class SidebarResponsiveComponent implements OnInit, AfterViewChecked{
 
+  sideBar : any;
+
+  constructor(private router: Router, private cookie: CookieService, private auth: AuthServiceService, private toast: NgToastService) {}
+  
+  ngAfterViewChecked(): void {
+    this.sideBar = document.getElementById('sadrzaj');
+    let height = window.innerHeight - 65.09;
+    console.log(height);
+    this.sideBar.style.height = height+'px';
+  }
+
+  ngOnInit(): void {
+  }
+
+  logout()
+  {
+    this.cookie.deleteAll();
+    this.router.navigate(["login"]);
+  }
 }
