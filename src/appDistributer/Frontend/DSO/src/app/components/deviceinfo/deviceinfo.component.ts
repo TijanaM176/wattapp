604dd5372f96f4831a4058a6d6e9cd3979cc428c
TijanaM176 <tijanamarkovic176@gmail.com>
2023-03-31 21:54:33 +0200

    #96 promenjen izgled stranice

--- src/appDistributer/Frontend/DSO/src/app/components/deviceinfo/deviceinfo.component.ts
+++ src/appDistributer/Frontend/DSO/src/app/components/deviceinfo/deviceinfo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
 import { ActivatedRoute } from '@angular/router';
+import { Device } from 'src/app/models/device';
 import { DeviceserviceService } from 'src/app/services/deviceservice.service';
 
 @Component({
@@ -15,8 +16,11 @@ export class DeviceinfoComponent implements OnInit{
   checked=false;
   currentUsage!:number;
   idDev!:string;
+  results: Device = new Device;
+  TypeName:any;
+  
   constructor( private router: ActivatedRoute, private service: DeviceserviceService){}
-  infoForm = new FormGroup({
+  /*infoForm = new FormGroup({
     IpAddress: new FormControl(''),
     TypeName: new FormControl(''),
     Manufacturer: new FormControl(''),
@@ -24,18 +28,19 @@ export class DeviceinfoComponent implements OnInit{
     MaxUsage: new FormControl(''),
     AvgUsage: new FormControl(''),
   });
-
+*/
   ngOnInit():void{
     this.idDev=this.router.snapshot.params['idDev'];
     this.service.getInfoDevice(this.idDev).subscribe((res:any)=>{
-      this.infoForm=new FormGroup({
+      this.results=res;
+      /*/this.infoForm=new FormGroup({
         IpAddress: new FormControl(res['IpAddress']),
         TypeName: new FormControl(res['TypeName']),
         Manufacturer: new FormControl(res['Manufacturer']),
         Name: new FormControl(res['Name']),
         MaxUsage: new FormControl(res['MaxUsage']),
         AvgUsage: new FormControl(res['AvgUsage']),
-      })
+      })*/
       this.currentUsage=res['CurrentUsage'];
     })
   }
