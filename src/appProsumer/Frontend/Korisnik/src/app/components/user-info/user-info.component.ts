7a4ebf7a5e6543b01eff332808679c30e50a214b
KatVir <94877518+KatVir@users.noreply.github.com>
2023-04-01 17:09:30 +0200

    #102 prikaz informacija korisnika na njegovom profilu
    koriscen je privremen id jednog od korisnika dok se na beku ne napravi izmena da se na front salje i id

--- src/appProsumer/Frontend/Korisnik/src/app/components/user-info/user-info.component.ts
+++ src/appProsumer/Frontend/Korisnik/src/app/components/user-info/user-info.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProsumerService } from 'src/app/services/prosumer.service';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.css']
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
 
+  username : string = '';
+  firstLastName : string = '';
+  email : string = '';
+  address : string = '';
+
+  privremeniId:string = '6e2e9369-1386-4be4-8690-a4b7545ecbce';
+
+  constructor(private prosumerService : ProsumerService) {}
+
+  ngOnInit(): void {
+    this.getInformation();
+  }
+
+  private getInformation()
+  {
+    this.prosumerService.getInforamtion(this.privremeniId)
+    .subscribe(
+      {
+        next:(res)=>{
+          this.username = res.username;
+          this.firstLastName = res.firstName+' '+res.lastName;
+          this.email = res.email;
+          this.address = res.address;
+        }
+      }
+    )
+  }
 }
---
+++ src/appProsumer/Frontend/Korisnik/src/app/services/prosumer.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProsumerService {
+
+  baseUrl : string = 'https://localhost:7156/api/Prosumer/';
+
+  constructor(private http:HttpClient) { }
+
+  getInforamtion(id : string):Observable<any>
+  {
+    return this.http.get<any>(this.baseUrl+'getProsumerByID?id='+id);
+  }
+}
