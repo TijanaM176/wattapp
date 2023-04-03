6b8504418177a19926dc200a1ba9dba8351b65a3
KatVir <94877518+KatVir@users.noreply.github.com>
2023-04-04 01:16:42 +0200

    #111 prikaz trenutne potrosnje i proizvodnje na sidebar-u home stranice aplikacije dso

--- src/appDistributer/Frontend/DSO/src/app/components/home-sidebar/home-sidebar.component.ts
+++ src/appDistributer/Frontend/DSO/src/app/components/home-sidebar/home-sidebar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeviceserviceService } from 'src/app/services/deviceservice.service';
 
 @Component({
   selector: 'app-home-sidebar',
   templateUrl: './home-sidebar.component.html',
   styleUrls: ['./home-sidebar.component.css']
 })
-export class HomeSidebarComponent {
+export class HomeSidebarComponent implements OnInit{
 
+  region : string = '';
+  currConsumption : string = '';
+  currProduction : string = '';
+  numOfUsers : string = '';
+
+  constructor(private deviceService : DeviceserviceService) {}
+
+  ngOnInit(): void {
+    this.getConsumptionProduction()
+  }
+
+  private getConsumptionProduction()
+  {
+    this.deviceService.getCurrConsumptionAndProduction()
+    .subscribe({
+      next:(res)=>{
+        this.currConsumption = res.totalConsumption;
+        this.currProduction = res.totalProduction;
+      }
+    })
+  }
 }
--- src/appDistributer/Frontend/DSO/src/app/services/deviceservice.service.ts
+++ src/appDistributer/Frontend/DSO/src/app/services/deviceservice.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,14 @@ import { Injectable } from '@angular/core';
 export class DeviceserviceService {
 
   constructor(private http:HttpClient) { }
-  private baseUrl: string = 'https://localhost:7156/GetDevice';
+
+  private baseUrl: string = 'https://localhost:7156/';
+
   getInfoDevice(id:string){
-    return this.http.get(`${this.baseUrl}` + `?id=` + id);
+    return this.http.get(`${this.baseUrl}GetDevice` + `?id=` + id);
+  }
+  getCurrConsumptionAndProduction():Observable<any>
+  {
+    return this.http.get<any>(this.baseUrl+'DsoSidebarInfo');
   }
 }
