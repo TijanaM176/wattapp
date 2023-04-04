3f1087af297908b747f71531c2d67cc9a414344b
KatVir <94877518+KatVir@users.noreply.github.com>
2023-04-04 23:46:51 +0200

    #114 cuvanje id-ja zaposlenog u kukiju

--- src/appDistributer/Frontend/DSO/src/app/components/login/login.component.ts
+++ src/appDistributer/Frontend/DSO/src/app/components/login/login.component.ts
@@ -68,6 +68,7 @@ export class LoginComponent implements OnInit {
             var decodedToken:any = jwt_decode(res.token);
             this.cookie.set('username',decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'].toString().trim());
             this.cookie.set('role',decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'].toString().trim());
+            this.cookie.set('id',decodedToken['sub'].toString().trim());
             this.cookie.set("token",res.token.toString().trim());
             this.cookie.set("refresh",res.refreshToken.toString().trim());
             console.log(this.cookie.get("refresh"));
