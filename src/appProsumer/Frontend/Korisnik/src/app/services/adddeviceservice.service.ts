939c868eb8963b0286ca908f3952b640c298b07a
TijanaM176 <tijanamarkovic176@gmail.com>
2023-04-06 02:38:21 +0200

    #117 odradjena stranica za registraciju uredjaja i povezana sa back-om

---
+++ src/appProsumer/Frontend/Korisnik/src/app/services/adddeviceservice.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdddeviceserviceService {
+
+  constructor(private http:HttpClient) { }
+  private baseUrl: string = 'https://localhost:7156/GetCategories';
+  private baseUrl1:string='https://localhost:7156/GetTypesByCategory';
+  private baseUrl2:string='https://localhost:7156/GetModels';
+  private baseUrl3:string='https://localhost:7156/RegisterDevice';
+  category!:number;
+  type!:number;
+  model!:string;
+  name!:string;
+  id!:string;
+  getCategories():Observable<any>{
+    return this.http.get<any>(this.baseUrl);
+  }
+  getTypes():Observable<any>{
+    return this.http.get<any>(this.baseUrl1+'?categoryId='+this.category);
+  }
+  getModels():Observable<any>{
+    return this.http.get<any>(this.baseUrl2+'?typeId='+this.type);
+  }
+  RegisterDevice():Observable<any>{
+    return this.http.get<any>(this.baseUrl3+'?prosumerId=='+this.id+'&modelId='+this.model+'&name='+this.name);
+  }
+}
