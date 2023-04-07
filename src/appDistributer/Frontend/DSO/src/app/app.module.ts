d945dc693cbfc08f5fce8b9deaa36cf08a5f47bd
Mihailo <sebekmihailo@gmail.com>
2023-04-07 18:26:32 +0200

    #128 uradjene kartice

--- src/appDistributer/Frontend/DSO/src/app/app.module.ts
+++ src/appDistributer/Frontend/DSO/src/app/app.module.ts
@@ -65,6 +65,7 @@ import { HistoryAllProsumersComponent } from './components/Charts/historyAllPros
 import { HomeSidebarComponent } from './components/home-sidebar/home-sidebar.component';
 import { WorkerProfileComponent } from './components/worker-profile/worker-profile.component';
 import { User1Component } from './components/user1/user1.component';
+import { PocetnaComponent } from './components/pocetna/pocetna.component';
 @NgModule({
   declarations: [
     AppComponent,
