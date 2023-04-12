f845758aae512ed5c12e12250670449919a30178
KatVir <94877518+KatVir@users.noreply.github.com>
2023-04-12 13:22:27 +0200

    #133 razlicitom bojom se boje kartice uredjaja koji su aktivni i koji nisu

--- src/appProsumer/Frontend/Korisnik/src/app/components/Charts/house/house.component.ts
+++ src/appProsumer/Frontend/Korisnik/src/app/components/Charts/house/house.component.ts
@@ -25,6 +25,13 @@ export class HouseComponent implements OnInit,AfterViewInit {
   }
   
   ngOnInit(): void {
+    
+    this.resizeObservable$ = fromEvent(window, 'resize');
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
+      const houseCont = document.getElementById('houseCont');
+      let houseHeight = this.widthService.height*0.65;
+      houseCont!.style.height = houseHeight + 'px';
+    });
   }
 
   setDevices(devices : any[], usages : { [key: string]: number })
