dea705b6af2c97606c866fb8ed19cad2ea766f60
KatVir <94877518+KatVir@users.noreply.github.com>
2023-04-18 01:14:30 +0200

    #156 uzimanje sa backend-a trenutne cene struje i prikaz na dashboard-u, centriranje teksta legende na graficima i promena boje neaktivnih uredjaja

--- src/appProsumer/Frontend/Korisnik/src/app/components/Pocetna/Pocetna.component.ts
+++ src/appProsumer/Frontend/Korisnik/src/app/components/Pocetna/Pocetna.component.ts
@@ -5,6 +5,7 @@ import { ProsumerService } from 'src/app/services/prosumer.service';
 import { CookieService } from 'ngx-cookie-service';
 import { HouseComponent } from '../Charts/house/house.component';
 import { DevicesStatusComponent } from '../Charts/devices-status/devices-status.component';
+import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
   selector: 'app-Pocetna',
@@ -14,20 +15,28 @@ import { DevicesStatusComponent } from '../Charts/devices-status/devices-status.
 export class PocetnaComponent implements OnInit, AfterViewInit {
   resizeObservable$!: Observable<Event>;
   resizeSubscription$!: Subscription;
+
   loader: boolean = true;
+
   devices: any[] = [];
   deviceUsages: { [key: string]: number } = {};
   numOfDevices: number = 0;
   numOfActiveDevices: number = 0;
+
   tariff: string = 'HIGHER';
+
   @ViewChild('house', { static: true }) house!: HouseComponent;
   @ViewChild('devicesStatus', { static: true })
+
   devicesStatus!: DevicesStatusComponent;
+
+  currentPrice : number = 0;
 
   constructor(
     private widthService: DeviceWidthService,
     private service: ProsumerService,
-    private cookie: CookieService
+    private cookie: CookieService,
+    private dashboardService : DashboardService
   ) {}
 
   ngAfterViewInit(): void {
@@ -40,6 +49,7 @@ export class PocetnaComponent implements OnInit, AfterViewInit {
       this.loader = false;
     }, 2000);
     this.getDevices();
+    this.getPrice()
     let hour = new Date().getHours();
     if (hour >= 22 || hour <= 6) {
       this.tariff = 'LOWER';
@@ -73,5 +83,18 @@ export class PocetnaComponent implements OnInit, AfterViewInit {
         this.numOfActiveDevices += 1;
       }
     });
+  }
+
+  getPrice()
+  {
+    this.dashboardService.getCurrentElecticityPrice()
+    .subscribe({
+      next:(res)=>{
+        this.currentPrice = res.Price;
+      },
+      error:(err)=>{
+        console.log(err.error);
+      }
+    })
   }
 }
